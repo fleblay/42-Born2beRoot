531b2407917f60cf93dce3825700487fad354f20
Fred <frederic.leblay@gmail.com>
2021-12-07 18:49:41 +0100

    Ajout debut du script

---
+++ monitoring.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MESSAGE_FINAL=""
+
+eval MESSAGE[0]=\$\'#Architecture : $(uname -s)\\n\'
+eval MESSAGE[1]=\$\'#CPU physical :\\n\'
+eval MESSAGE[2]=\$\'#vCPU :\\n\'
+eval MESSAGE[3]=\$\'#Memory Usage :\\n\'
+
+for i in $(seq 0 $((${#MESSAGE[@]} - 1)))
+do
+	MESSAGE_FINAL="${MESSAGE_FINAL}${MESSAGE[$i]}"
+done
+
+echo "$MESSAGE_FINAL"
+
+#echo "${MESSAGE[@]}"
+#echo "${MESSAGE[*]}"
